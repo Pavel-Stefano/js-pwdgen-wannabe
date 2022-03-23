c983dacb4b961a2eac00559ce145d10d10c6f27d
Pavel-Stefano <pavelstefanominuti@gmail.com>
2022-03-23 15:58:04 +0100

    index: js script

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// Chiedi all’utente il suo nome,
+// poi chiedi il suo cognome,
+// poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina nomecognomecolorepreferito21
+
+let userName = prompt('come ti chiami?')
+console.log(userName)
+
+document.getElementById('userName').innerHTML += userName;
+
+
+let lastName = prompt('quale è il tuo cognome?')
+console.log(lastName)
+
+document.getElementById('lastName').innerHTML += lastName;
+
+
+
+let favoriteColor = prompt('quale è il tuo colore preferito?')
+console.log(favoriteColor)
+
+document.getElementById('favoriteColor').innerHTML += favoriteColor;
+
+
+const riassunto = ('')
+document.getElementById('riassunto').innerHTML = userName + '' + lastName + '' +favoriteColor + '' + 21;
+
